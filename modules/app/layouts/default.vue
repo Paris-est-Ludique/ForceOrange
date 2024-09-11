c35de52aece7eb66517cda55108b8b661a27a28d
ChatonDeAru <823314+chatondearu@users.noreply.github.com>
2024-09-11 19:51:06 +0200

    🎨 Working app and shared with start of pages

---
+++ modules/app/layouts/default.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+  bodyAttrs: {
+    class: 'bg-[#F9DD75F2]'
+  }
+})
+const links = [{
+  label: 'Les actus',
+  to: '/'
+}, {
+  label: 'Rejoindre FO',
+  to: '/join'
+}]
+</script>
+
+<template>
+  <UHeader :links="links" class="rounded-xl shadow-lg bg-white mx-4 mt-4">
+    <template #left>
+      <!-- <p class="font-logo text-orange-500 stroke-5 stroke-black-500 text-hlogo">Force Orange</p> -->
+      <NuxtImg src="/assets/img/logo-fo.svg" alt="Force Orange" />
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+      <UButton to="/signin" variant="soft">Se connecter</UButton>
+    </template>
+  </UHeader>
+
+  <UMain class="m-4">
+    <slot />
+  </UMain>
+
+  <UNotifications />
+</template>
