ff79ea88ff367f1dfa884ff6c899a31b6fc7cdbc
Simon <simon.priet@gmail.com>
2024-10-16 19:39:09 +0200

    fix(#43): Fix 2 grammar issues on the join page

--- modules/app/pages/join/index.vue
+++ modules/app/pages/join/index.vue
@@ -140,7 +140,7 @@ async function onError(event: FormErrorEvent) {
       </div>
 
       <UFormGroup name="lastEdition" class="my-8">
-        <UCheckbox label="J'ai participer à la précédente édition ?" v-model="state.lastEdition" />
+        <UCheckbox label="J'ai participé la précédente édition ?" v-model="state.lastEdition" />
         <template #help>
           Dis nous si tu étais déjà bénévole à la dernière édition.<br>
           Surtout ne mens pas, on vous connait ! (et on a les photos)
