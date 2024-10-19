a1ce17e559e97589d5f9109d61dde34736130b66
ChatonDeAru <823314+chatondearu@users.noreply.github.com>
2024-10-19 15:42:29 +0200

    🚧 Prepare for members pages and perms on auth users

---
+++ modules/app/pages/members/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+definePageMeta({
+  name: 'Members',
+  middleware: 'admin',
+})
+</script>
+
+<template>
+  <WIPNotif />
+  <UCard class="container mx-auto max-w-screen-md">
+    <h1 class="text-2xl uppercase">Bienvenue sur Force Orange</h1>
+    <p class="font-light">
+      Tu retrouveras toutes les informations nécessaires pour t'accompagner dans la préparation du festival Paris est
+      Ludique 2025.
+    </p>
+  </UCard>
+</template>
